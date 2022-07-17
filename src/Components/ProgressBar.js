e11cfa54f4cd28f8752be3ed219d40446683610b
Wasiq Ali <wasiq@itcentre.pk>
2022-07-17 12:14:49 +0500

    firstcomit

---
+++ src/Components/ProgressBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import * as Progress from 'react-native-progress';
+export default class ProgressBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Progress.Bar borderColor='white' color='#5544ab' unfilledColor='#e6e6e6' borderRadius={10} progress={0.3} width={null}  height={12} style={{marginHorizontal:20}}/>
+      </View>
+    );
+  }
+}
